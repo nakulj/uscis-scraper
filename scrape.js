8df35440101493b5fa64c2bb67d1af53df7a6a6a
nakulj <3213360+nakulj@users.noreply.github.com>
2023-03-14 00:00:27 -0700

    cleanup

--- scrape.js
+++ scrape.js
@@ -1,29 +1,23 @@
 const puppeteer = require('puppeteer');
 const caseNumber = process.argv[2]; // node scrape.js WACXXXX
 
-new Promise(async (resolve, reject) => {
-  try {
-    return await scrape(resolve);
-  } catch (e) {
-    return reject(e);
-  }
-}).then(console.log).catch(console.error);
-
-async function scrape(resolve) {
+(async () => {
   const browser = await puppeteer.launch();
+  h = await scrapeCase(browser);
+  console.log(h)
+  await browser.close();
+})();
+
+async function scrapeCase(browser) {
   const page = await browser.newPage();
 
   await page.goto("https://egov.uscis.gov/casestatus/landing.do");
   await page.evaluate(cn => document.querySelector('#receipt_number').value = cn, caseNumber);
 
   // clicking causes page navigation, don't proceed until that is finished
-  await Promise.all([
-    page.waitForNavigation(),
-    page.evaluate(() => document.querySelector('input[type=submit]').click()),
-  ]); 
+  let nav = page.waitForNavigation();
+  page.click('input[type=submit]');
+  await nav;
 
-  innerHtml = await page.evaluate(() => document.querySelector('div.rows.text-center').innerHTML);
-  browser.close();
-  return resolve(innerHtml);
+  return await page.$eval('div.rows.text-center', e => e.innerHTML);
 }
-
